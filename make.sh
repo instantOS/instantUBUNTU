befb3aac5074297299efb9bf47329d601be60b97
Robin <dequanzh@usc.edu>
2020-07-27 21:01:25 -0700

    tqdm makes right

--- make.sh
+++ make.sh
@@ -8,9 +8,15 @@ install() {
         exit
     fi
     rm -f "$logfile" && touch "$logfile"
-    logdo "instantOS Installation Begin"
+    logdo "Installation Begin @ $(date -I)-$(date +"%T.%3N")"
     # Tasks
     {
+        download bootstrap
+        download download-tools
+        download media-player
+        download text-editor
+        download input-method
+        download build-tools
         download aptPackages
         compile instantOSPackages
     } |& echo_progess
@@ -32,25 +38,50 @@ compile() {
     done
 }
 
+bootstrap() {
+    apt update
+    # apt install python3-tqdm
+    pip3 install -U 'git+https://github.com/tqdm/tqdm@cli-tee#egg=tqdm'
+}
+
 aptPackages() {
-    apt install -y fzf expect git os-prober dialog imvirt lshw bash curl python3-tqdm
+    apt install -y fzf expect git os-prober dialog imvirt lshw bash curl
     apt install -y dunst lxpolkit xdotool compton lightdm
+}
+
+media-player() {
     apt install -y mpv mpd mpc slop maim mupdf xwallpaper #muti-media
+}
+
+text-editor() {
+    apt install vim
     apt install -y scite # gui super lightweight gui text editor (alt. featherpad)
+}
+
+input-method() {
     apt install -y fcitx fcitx-libpinyin # input method framework
-    apt install -y build-essential libx11-dev xorg-dev # build tools for windows manager
+}
+
+download-tools() {
+    apt install curl wget
+}
+
+build-tools() {
+    # for tools need be compiled from sources (e.g. windows manager)
+    apt install -y build-essential libx11-dev xorg-dev
+    apt install -y libimlib2-dev
 }
 
 instantOSPackages() {
       git submodule update --init --recursive
-      cd-do instawm/ make install -j$(nproc)
-      cd-do instantDEB/ ./make.sh download
-      cd-do instantDEB/ ./make.sh unpack
-      cd-do instantDEB/ cp -r usr /usr
-      cd-do instantDEB/ cp -r etc /etc
+      cd_do instawm/ make install -j$(nproc)
+      cd_do instantDEB/ ./make.sh download
+      cd_do instantDEB/ ./make.sh unpack
+      cd_do instantDEB/ cp -r usr /usr
+      cd_do instantDEB/ cp -r etc /etc
 }
 
-cd-do() {
+cd_do() {
     cd "$1"
     shift 1
     "$@"
@@ -60,17 +91,7 @@ cd-do() {
 # read from stdin
 echo_progess() {
     if command -v tqdm >& /dev/null ; then
-        # tqdm --total 12 --desc "${input}\nProgress" > /dev/null
-        python3 <(cat <<EOF
-from tqdm import tqdm
-import time
-import fileinput
-t = tqdm(fileinput.input(), desc="Progress", total=2)
-for text in t:
-    if text.startswith('[instantOS]'):
-        t.write(text.strip())
-EOF
-)
+        tqdm --tee --total 8 --desc "Progress" > /dev/null
     else
         xargs -l1 echo
     fi
@@ -82,13 +103,13 @@ logdo() {
     local -r desc="$1"
     shift 1
     local -r cmd="$@"
-    local -r message=$(echo [instantOS][$(date -I)-$(date +"%T.%3N")] "$desc" "$cmd")
+    local -r message=$(echo [instantOS][$(date +"%T.%3N")] "$desc" "$cmd")
     echo -n "$message" | tee -a "$logfile" ; echo >> "$logfile"
     if test ! -z "$cmd"
     then
         "$cmd" >> $logfile 2>&1  &
         local -r pid="$!"
-        spinner "$pid"
+        # spinner "$pid"
         echo
         wait "$pid" #capture exit code
         return $?
@@ -118,4 +139,9 @@ restoreState() {
 
 trap restoreState EXIT
 
-"$@"
+if [ -z $1 ] ; then
+    echo "Usage:"
+    echo "sudo ./make.sh install"
+else
+    "$@"
+fi
